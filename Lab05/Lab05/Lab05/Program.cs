2e5c5a4763b21b4036c7ea61bc87362216a6e3ea
Vlad <vlad.mishh@gmail.com>
2022-05-31 14:20:12 +0300

    Lab05-06(without scheme)

---
+++ Lab05/Lab05/Lab05/Program.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using static System.Console;
+
+namespace Lab05
+{
+    public class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            int n = 0, m = 0;
+
+        x1:
+            try
+            {
+                Write("M -> ");
+                m = int.Parse(ReadLine());
+                if (m <= 0) throw new Exception("'M' must be positive");
+
+            x2:
+                try
+                {
+                    Write("N -> ");
+                    n = int.Parse(ReadLine());
+                    if (n <= 0) throw new Exception("'N' must be positive");
+
+                }
+                catch (Exception e)
+                {
+                    WriteLine(e.Message);
+                    ReadLine();
+                    Clear();
+                    goto x2;
+                }
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.Message);
+                ReadLine();
+                Clear();
+                goto x1;
+            }
+
+        x3:
+            Clear();
+            WriteLine("type 'r' to get random values");
+            WriteLine("type 'h' to set values by hand");
+            Write("-> ");
+
+            string decision = ReadLine().ToLower();
+
+            int[,] array = new int[m, n];
+
+            switch (decision)
+            {
+                case "r":
+                    for (int i = 0; i < m; i++)
+                        for (int j = 0; j < n; j++)
+                            array[i, j] = rnd.Next(50);
+                    Clear();
+                    break;
+                case "h":
+                    for (int i = 0; i < m; i++)
+                        for (int j = 0; j < n; j++)
+                        {
+                        x4:
+                            Write("array[{0},{1}] -> ", i, j);
+                            try
+                            {
+                                array[i, j] = int.Parse(ReadLine());
+                            }
+                            catch (Exception e)
+                            {
+                                WriteLine(e.Message);
+                                Clear();
+                                goto x4;
+                            }
+                            Clear();
+                        }
+                    break;
+                default:
+                    goto x3;
+            }
+
+            WriteLine("{0}x{1} array: \n", m, n);
+
+            int elements = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((j < i && i < n - 1 - j))
+                    {
+                        ForegroundColor = ConsoleColor.Red;
+                        elements++;
+                    }
+                    Write(array[i, j] + "\t");
+                    ForegroundColor = ConsoleColor.White;
+                }
+                WriteLine("\n");
+            }
+
+            int[] sortPart = new int[elements];
+
+            int counter = 0;
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if ((j < i && i < n - 1 - j))
+                    {
+                        sortPart[counter] = array[i, j];
+                        counter++;
+                    }
+                }
+
+            WriteLine("\n elements to sort:");
+            foreach (int i in sortPart)
+                Write(i + " ");
+
+            sortPart = Sort(sortPart, m, 0, sortPart.Length - 1);
+
+
+            WriteLine("\n\n sorted elements:");
+            foreach (int i in sortPart)
+                Write(i + " ");
+            WriteLine("\n");
+
+            WriteLine("\n");
+
+            int J = 0;
+            int I = 0;
+            int count = 0;
+            bool moveUp = false;
+            bool put = false;
+            while (count != sortPart.Length)
+            {
+                put = false;
+
+
+                if (J < I && I < n - 1 - J)
+                {
+
+                    array[I, J] = sortPart[count];
+                    count++;
+                    
+                }
+
+
+
+                if (I == 0 && moveUp == true)
+                {
+                    J++;
+                    moveUp = false;
+                    put = true;
+                }
+                else if (I == m - 1 && moveUp == false)
+                {
+                    J++;
+                    moveUp = true;
+                    put = true;
+                }
+
+                if(put == false)
+                {
+                    if (moveUp == false) I++;
+                    else I--;
+                }
+                
+            }
+
+
+
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((j < i && i < n - 1 - j))
+                    {
+                        ForegroundColor = ConsoleColor.Green;
+                    }
+                    Write(array[i, j] + "\t");
+                    ForegroundColor = ConsoleColor.White;
+                }
+                WriteLine("\n");
+            }
+
+            ReadLine();
+        }
+
+        static int[] Sort(int[] array, int M, int start, int end)
+        {
+
+
+            if (end - start + 1 < M)
+            {
+
+                for (int i = start; i < end + 1; i++)
+                {
+                    int key = array[i];
+                    int j = i - 1;
+                    while (j >= 0 && array[j] < key)
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                    }
+                    array[j + 1] = key;
+                }
+                return array;
+            }
+            else
+            {
+                int left = array[start];
+                int right = array[end];
+                int middle = array[(end - start + 1) / 2];
+
+                if ((left > middle && middle > right) || (left < middle && middle < right))
+                {
+                    int buffer = middle;
+                    middle = right;
+                    right = buffer;
+                }
+                if ((left > middle && left < right) || (left < middle && left > right))
+                {
+                    int buffer = left;
+                    left = right;
+                    right = buffer;
+                }
+
+                int i = start;
+                int j = end - 1;
+                bool i_r = false;
+                bool j_r = false;
+                while (!(i >= j))
+                {
+                    if (array[i] > right)
+                    {
+                        i_r = true;
+                    }
+                    else i++;
+                    if (array[j] < right)
+                    {
+                        j_r = true;
+                    }
+                    else j--;
+
+                    if (j_r == i_r == true)
+                    {
+                        int buffer = array[i];
+                        array[i] = array[j];
+                        array[j] = buffer;
+                        j_r = false;
+                        i_r = false;
+                        i++;
+                        j--;
+                    }
+                }
+
+
+                if (array[i] < array[end])
+                {
+                    int buffer = array[i];
+                    array[i] = array[end];
+                    array[end] = buffer;
+                }
+
+                int pivot_i = i;
+
+                array = Sort(array, M, start, pivot_i);
+                array = Sort(array, M, pivot_i + 1, end);
+
+                return array;
+            }
+        }
+    }
+}
---
+++ Lab06/dequeBy3Stacks/dequeBy3Stacks/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using static System.Console;
+
+namespace Lab06
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Deque myDeque = new Deque();
+            Write("your text -> ");
+            string text = ReadLine();
+            foreach (char c in text)
+                myDeque.InsertHead(c + "");
+
+            if (CheckIfPalindrome(myDeque) == true)
+            {
+                WriteLine("writen text is palindrome:");
+                myDeque.Print();
+            }
+            else
+            {
+                convertToPalindrome(myDeque);
+            }
+            ReadLine();
+        }
+        static void convertToPalindrome(Deque inputDeque)
+        {
+            Deque invertDeque = new Deque();
+            Deque bufferDeque = new Deque();
+
+            while (inputDeque.CheckIfEmpty() != true)
+            {
+                string element = inputDeque.PopTail();
+                invertDeque.InsertHead(element);
+                bufferDeque.InsertHead(element);
+            }
+
+            while (bufferDeque.CheckIfEmpty() != true)
+                inputDeque.InsertHead(bufferDeque.PopTail());
+
+            invertDeque.Print();
+            while (CheckIfPalindrome(inputDeque) != true)
+            {
+                if (inputDeque.peekHead() != invertDeque.peekHead())
+                {
+                    inputDeque.InsertHead(invertDeque.PopHead());
+                    inputDeque.Print();
+                }
+                else invertDeque.PopHead();
+            }
+
+        }
+        static bool CheckIfPalindrome(Deque inputDeque)
+        {
+            Deque checkDeque = new Deque();
+            Deque bufferDeque = new Deque();
+
+            while (inputDeque.CheckIfEmpty() != true)
+            {
+                string element = inputDeque.PopTail();
+                checkDeque.InsertHead(element);
+                bufferDeque.InsertHead(element);
+            }
+
+            while (bufferDeque.CheckIfEmpty() != true)
+                inputDeque.InsertHead(bufferDeque.PopTail());
+
+            while (checkDeque.Length() > 1)
+            {
+                if (checkDeque.PopTail() != checkDeque.PopHead())
+                    return false;
+            }
+            return true;
+        }
+    }
+    public class Deque
+    {
+        public Stack head;
+        public Stack tail;
+        private Stack buffer;
+
+        public Deque()
+        {
+            head = new Stack();
+            tail = new Stack();
+            buffer = new Stack();
+        }
+        public void InsertHead(string value)
+        {
+            this.head.Push(value);
+        }
+        public void InsertTail(string value)
+        {
+            this.tail.Push(value);
+        }
+
+        public bool CheckIfEmpty()
+        {
+            if (head.isEmpty == true && tail.isEmpty == true)
+                return true;
+            else return false;
+        }
+        public string PopTail()
+        {
+            if (this.CheckIfEmpty() == false)
+                if (tail.isEmpty == true)
+                {
+                    while (head.isEmpty == false)               // buffer -> head -> tail
+                        buffer.Push(head.Pop());              // buffer -> head
+                    for (int i = 0; i < buffer.Length(); i++)
+                        head.Push(buffer.Pop());
+                    while (head.isEmpty == false)
+                        tail.Push(head.Pop());
+                    while (buffer.isEmpty == false)
+                        head.Push(buffer.Pop());
+                    return tail.Pop();
+                }
+            return tail.Pop();
+        }
+        public string PopHead()
+        {
+            if (this.CheckIfEmpty() == false)
+                if (head.isEmpty == true)
+                {
+                    while (tail.isEmpty == false)               // buffer -> tail -> head
+                        buffer.Push(tail.Pop());               // buffer -> tail
+                    for (int i = 0; i < buffer.Length(); i++)
+                        tail.Push(buffer.Pop());
+                    while (tail.isEmpty == false)
+                        head.Push(tail.Pop());
+                    while (buffer.isEmpty == false)
+                        tail.Push(buffer.Pop());
+                    return head.Pop();
+                }
+            return head.Pop();
+        }
+        public string peekHead()
+        {
+            string value = head.Pop();
+            head.Push(value);
+            return value;
+        }
+        public void Print()
+        {
+            while (head.isEmpty == false)
+                buffer.Push(head.Pop());
+            while (tail.isEmpty == false)
+                head.Push(tail.Pop());
+            while (head.isEmpty == false)
+                buffer.Push(head.Pop());
+
+            buffer.Print();
+
+            for (int i = 0; i < buffer.Length(); i++)
+                head.Push(buffer.Pop());
+            while (tail.isEmpty == false)
+                tail.Push(buffer.Pop());
+            while (buffer.isEmpty == false)
+                head.Push(buffer.Pop());
+        }
+
+        public int Length()
+        {
+            return (buffer.Length() + tail.Length() + head.Length());
+        }
+    }
+    public class Stack
+    {
+        private StackElement tail;
+        public bool isEmpty;
+        public Stack()
+        {
+            this.tail = null;
+            this.isEmpty = true;
+        }
+        public void Push(string value)
+        {
+            if (isEmpty == true)
+            {
+                StackElement newTail = new StackElement(value, null);
+                tail = newTail;
+                isEmpty = false;
+            }
+            else
+            {
+                StackElement newTail = new StackElement(value, tail);
+                tail = newTail;
+            }
+        }
+        public int Length()
+        {
+            int length = 0;
+            if (isEmpty == true) return length;
+            else if (tail.GetOrigin() == null) return 1;
+            else
+            {
+                length++;
+                StackElement current = this.tail;
+                while (current.GetOrigin() != null)
+                {
+                    length++;
+                    current = current.GetOrigin();
+                }
+                return length;
+            }
+        }
+        public void Clear()
+        {
+            Stack tail = null;
+            isEmpty = true;
+        }
+        public string Pop()
+        {
+            if (isEmpty == false)
+            {
+                string tailValue = this.tail.GetValue();
+                if (tail.GetOrigin() == null)
+                {
+                    this.Clear();
+                }
+                else
+                {
+                    this.tail = tail.GetOrigin();
+                }
+                return tailValue;
+            }
+            else return null;
+        }
+        public void Print()
+        {
+            if (isEmpty == true) WriteLine("Stack is empty");
+            else
+            {
+                StackElement current = tail;
+                Write("[{0}]", current.GetValue());
+                while (current.GetOrigin() != null)
+                {
+                    current = current.GetOrigin();
+                    Write("[{0}]", current.GetValue());
+                }
+                WriteLine();
+            }
+        }
+    }
+
+    public class StackElement
+    {
+        private string value;
+        private StackElement origin;
+        public StackElement(string value, StackElement origin)
+        {
+            this.value = value;
+            this.origin = origin;
+        }
+
+        public string GetValue()
+        {
+            return value;
+        }
+        public StackElement GetOrigin()
+        {
+            return origin;
+        }
+    }
+}
